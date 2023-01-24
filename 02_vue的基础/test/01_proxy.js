d8348c2c9d1fa56f06a975cdd6d45ca8626ec1c8
lidongze <ldzttkx@qq.com>
2023-01-24 20:48:12 +0800

    update

--- 02_vue的基础/test/01_proxy.js
+++ 02_vue的基础/test/01_proxy.js
@@ -12,10 +12,36 @@ const obj = {
 */
 
 // 为对象创建代理
-const handler = {}  // 用来指定代理行为
+const handler = {
+
+    // get用来指定读取数据时的行为，它的返回值就是最终读取到的值
+    // 指定get后,在通过代理读取对象属性时,就会调用get方法来获取值
+    get(target,prop,receiver){
+
+        // 在vue中,data()返回的对象都会被vue代理
+        // vue代理后,当我们通过代理去读取属性时,返回值之前,它会先做一个跟踪的操作
+        //      当我们通过代理去修改属性时,修改后会通知之前所有用到该值的位置进行更新
+        
+
+        /* 
+            三个参数:
+                1. target 被代理的对象
+                2. prop 读取的属性
+                3. receiver 代理对象
+        */
+        return target[prop]
+    },
+
+    // set会在通过代理修改对象时调用
+    set(target,prop,value,receiver){
+        target[prop] = value
+
+        // trigger()会出发所有的使用该值的位置进行更新
+    }
+}  // 用来指定代理行为
 
 // 创建代理
 const proxy = new Proxy(obj,handler)
 
 proxy.name = '猪八戒'
-console.log(obj.name);
+console.log(proxy.name);
